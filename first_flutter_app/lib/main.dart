f1662a3fd1f6fac1db6cac10b7cf396db8521f7a
Jayanta <bjayanta.me@gmail.com>
2022-03-29 11:54:40 +0600

    add all lecture

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,6 +1,71 @@
+import 'dart:math';
+
 import 'screens/Dashboard.dart';
 import 'package:flutter/material.dart';
 
+// Lecture 1:
+// void main() {
+//   runApp(Text(
+//     "Hello world!",
+//     textDirection: TextDirection.ltr,
+//   ));
+// }
+
+// Lecture: 2
+// void main() {
+//   runApp(Center(
+//     child: Text(
+//       "Hello world!",
+//       textDirection: TextDirection.ltr,
+//     ),
+//   ));
+// }
+
+// Lecture: 3
+// void main() {
+//   runApp(MaterialApp(
+//     title: "First App",
+//     home: Center(
+//       child: Text(
+//         "Hello world!",
+//         textDirection: TextDirection.ltr,
+//       ),
+//     ),
+//   ));
+// }
+
+// Lecture: 4
+// void main() {
+//   runApp(MaterialApp(
+//     title: "First App",
+//     debugShowCheckedModeBanner: false,
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: Text("First app"),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text.rich(
+//               TextSpan(text: "My ", children: [
+//                 TextSpan(text: "First ", style: TextStyle(fontSize: 32.0, color: Colors.redAccent, fontWeight: FontWeight.bold,),),
+//                 TextSpan(text: "App", style: TextStyle(fontSize: 24.0, color: Colors.lightGreen,),),
+//               ]),
+//             ),
+//             Text("Current random value is: ${getNextInt()}"),
+//           ],
+//         ),
+//       ),
+//     )
+//   ));
+// }
+//
+// int getNextInt() {
+//   return Random().nextInt(100);
+// }
+
+// Lecture: 5
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
